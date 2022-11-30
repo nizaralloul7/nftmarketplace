97dc14193e47f5feeb30954177f05897c66b1f91
nizaralloul7 <nizaralloul7@outlook.fr>
2022-11-30 19:00:18 +0100

    last commit

--- app/src/main/java/com/example/nftmarketplace/MainActivity.java
+++ app/src/main/java/com/example/nftmarketplace/MainActivity.java
@@ -37,44 +37,12 @@ public class MainActivity extends AppCompatActivity
         bottomNavigationView.setSelectedItemId(R.id.home);
         SharedPreferences sharedpreferences = getSharedPreferences(LoginActivity.MyPREFERENCES, Context.MODE_PRIVATE);
 
-        Log.i("My Logger",sharedpreferences.getString("userName","error"));
         User user1 = new User("Nizar", "Alloul", "Shigur","motdepasse123","nizaralloul7@gmail.com",120.04,R.drawable.profile1);
         User user2 = new User("Othmane", "Lz", "TfBlade","motdepasse123","othmane@gmaile.com",111.00,R.drawable.profile2);
         User user3 = new User("Mounia", "Ech", "MEchel","motdepasse123","mounia@gmail.com",314.54,R.drawable.profile3);
         User user4 = new User("Basma", "Haimeur", "Smilie","motdepasse123","basma.haimeur@gmail.com",102.01,R.drawable.anothermonkey);
         User user5 = new User("Yahya", "Bjrh", "Ybjrh","motdepasse123","Yahya.boujrah@gmail.com",87.14,R.drawable.profile4);
-/*
-        db.insertUser(user1);
-        db.insertUser(user2);
-        db.insertUser(user3);
 
-        User user = db.getUser("test");
-
-        NFTModel[] nftModels = new NFTModel[]
-                {
-                        new NFTModel("3.18", "3714,40USD", R.drawable.nft1,"RydenHat-15",user,"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut mauris a lorem pulvinar sollicitudin non nec neque. Nullam ultricies laoreet arcu quis consequat. Vestibulum ac est scelerisque, consequat lorem id, feugiat sem."),
-                        new NFTModel("4.85", "5665,04USD", R.drawable.nft2,"RydenSam-05",user2,"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut mauris a lorem pulvinar sollicitudin non nec neque. Nullam ultricies laoreet arcu quis consequat. Vestibulum ac est scelerisque, consequat lorem id, feugiat sem."),
-                        new NFTModel("8.01", "9356,08USD", R.drawable.nft3,"JhinGlas-X",user,"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut mauris a lorem pulvinar sollicitudin non nec neque. Nullam ultricies laoreet arcu quis consequat. Vestibulum ac est scelerisque, consequat lorem id, feugiat sem."),
-                        new NFTModel("2.54", "2966,85USD", R.drawable.nft5,"RydenX14-02",user1,"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut mauris a lorem pulvinar sollicitudin non nec neque. Nullam ultricies laoreet arcu quis consequat. Vestibulum ac est scelerisque, consequat lorem id, feugiat sem."),
-                        new NFTModel("3.71", "4333,47USD", R.drawable.nft6,"RydenZW2-14",user,"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut mauris a lorem pulvinar sollicitudin non nec neque. Nullam ultricies laoreet arcu quis consequat. Vestibulum ac est scelerisque, consequat lorem id, feugiat sem."),
-                        new NFTModel("4.02", "4695,56USD", R.drawable.rebels,"Ryden71-03",user3,"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut mauris a lorem pulvinar sollicitudin non nec neque. Nullam ultricies laoreet arcu quis consequat. Vestibulum ac est scelerisque, consequat lorem id, feugiat sem."),
-                        new NFTModel("3.14", "3667,68USD", R.drawable.ryden_g2,"RydenX1-22",user1,"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut mauris a lorem pulvinar sollicitudin non nec neque. Nullam ultricies laoreet arcu quis consequat. Vestibulum ac est scelerisque, consequat lorem id, feugiat sem."),
-                        new NFTModel("3.18", "3714,40USD", R.drawable.ryden,"RydenX2-28",user,"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut mauris a lorem pulvinar sollicitudin non nec neque. Nullam ultricies laoreet arcu quis consequat. Vestibulum ac est scelerisque, consequat lorem id, feugiat sem."),
-                        new NFTModel("4.51", "5267,91USD", R.drawable.overstudy_g1,"OverStudy-5v2",user1,"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut mauris a lorem pulvinar sollicitudin non nec neque. Nullam ultricies laoreet arcu quis consequat. Vestibulum ac est scelerisque, consequat lorem id, feugiat sem."),
-                        new NFTModel("4.55", "5314,63USD", R.drawable.arthilarius_g1,"ArtHilarious-g2",user2,"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut mauris a lorem pulvinar sollicitudin non nec neque. Nullam ultricies laoreet arcu quis consequat. Vestibulum ac est scelerisque, consequat lorem id, feugiat sem."),
-                        new NFTModel("7.92", "9250,96USD", R.drawable.nft4,"KarthUXW-g14",user,"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut mauris a lorem pulvinar sollicitudin non nec neque. Nullam ultricies laoreet arcu quis consequat. Vestibulum ac est scelerisque, consequat lorem id, feugiat sem."),
-                        new NFTModel("12.01", "14028,28USD", R.drawable.a7d84f35d60e345fd2f70d053a6d01b7,"ZED-g14",user2,"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut mauris a lorem pulvinar sollicitudin non nec neque. Nullam ultricies laoreet arcu quis consequat. Vestibulum ac est scelerisque, consequat lorem id, feugiat sem."),
-                };
-
-        */
-
-       /* for(NFTModel nftModel : nftModels)
-        {
-            boolean result = db.insertNFT(nftModel);
-            Log.i("inserted",String.valueOf(result));
-        }
-
-        */
         User[] users = new User[]
                 {
                         user1,
